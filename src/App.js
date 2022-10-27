052faaddd366e8d0085442e0cad0b87503f45937
abdibnu <46236467+abdibnu@users.noreply.github.com>
2022-10-27 22:58:49 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [title, setTitle] = useState("");
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "To Do List",
+    },
+  ]);
+  return (
+    <div className="container">
+      <input
+        type="text"
+        value={title}
+        onChange={(event) => {
+          setTitle(event.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          setTodos([...todos, { id: todos.length + 1, title: title }]);
+        }}
+      >
+        Add List
+      </button>
+
+      <h1>Todo List</h1>
+      <div className="todos-container">
+        {todos.map((todo) => (
+          <div className="todo" key={todo.id}>
+            {todo.title}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
